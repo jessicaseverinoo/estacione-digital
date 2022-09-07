4422537fd363103e9814b026281893455c425294
Jessica Severino <jessicaa.severinoo@gmail.com>
2022-09-07 12:48:18 -0300

    change name title app to Estacione Digital

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Estacione Digital',
       theme: lightTheme,
       home: const App(),
     );
